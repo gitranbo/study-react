abdd7c8bb86dee3ba0fd0352b740967dc3af3b1c
choma <xx@gmail.com>
2023-04-24 08:54:38 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを記述しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,25 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
     <div className={classes.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
