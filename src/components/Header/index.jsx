c3114ade25d0b881b757bc293b863e764b8072d6
choma <xx@gmail.com>
2023-04-26 10:26:47 +0900

    ♻️ リファクタリング

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
 
-export function Header() {
+const NAV_ITEMS = [
+  { href: "/", label: "Index" },
+  { href: "/about", label: "About" },
+];
+
+export const Header = () => {
   return (
     <header className={classes.header}>
-      <Link href="/" className={classes.anchor}>
-        <div>Index</div>
-      </Link>
-      <Link href="/about" className={classes.anchor}>
-        <div>About</div>
-      </Link>
+      {NAV_ITEMS.map((item) => {
+        return (
+          <Link key={item.href} href={item.href} className={classes.anchor}>
+            <div>{item.label}</div>
+          </Link>
+        );
+      })}
     </header>
   );
-}
+};
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,16 +4,7 @@ import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-export default function About({
-  doubleCount,
-  isShow,
-  handleClick,
-  handleDisplay,
-  text,
-  array,
-  handleChange,
-  handleAdd,
-}) {
+const About = (props) => {
   return (
     <div className={classes.container}>
       <Head>
@@ -21,14 +12,16 @@ export default function About({
       </Head>
       <Header />
 
-      {isShow ? <h1>{doubleCount}</h1> : null}
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+      <button onClick={props.handleClick}>ボタン</button>
+      <button onClick={props.handleDisplay}>
+        {props.isShow ? "非表示" : "表示"}
+      </button>
 
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>追加</button>
       <ul>
-        {array.map((item) => {
+        {props.array.map((item) => {
           return <li key={item}>{item}</li>;
         })}
       </ul>
@@ -37,4 +30,5 @@ export default function About({
       <Footer />
     </div>
   );
-}
+};
+export default About;
